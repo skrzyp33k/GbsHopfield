2ae913fd92f6400bcaa2a89dbec10babd3dbc55e
skrzyp33k <skrzyp33k@protonmail.com>
2022-07-31 13:47:17 +0200

    gbs commit

---
+++ HopfieldSSEditor/MainWindow.xaml.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace HopfieldSSEditor
+{
+    public partial class MainWindow : Window
+    {
+        private const int N = 9; //rows
+        private const int M = 9; //columns
+        private const int PixelSize = 60;
+
+        private bool[,] pixels;
+
+        private Button[,] button;
+
+        private void colorButtons()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    if (pixels[i, j])
+                    {
+                        button[i, j].Background = new SolidColorBrush(Colors.Black);
+                    }
+                    else
+                    {
+                        button[i, j].Background = new SolidColorBrush(Colors.White);
+                    }
+                }
+            }
+        }
+
+        private void pixelButtonClicked(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+
+            string[] str = b.Name.Split('_');
+
+            int i = Int32.Parse(str[1]);
+            int j = Int32.Parse(str[2]);
+
+            pixels[i, j] = !pixels[i, j];
+
+            colorButtons();
+        }
+
+        private void buildStructure()
+        {
+            MWindow.Title = "HopfieldSS Editor " + N.ToString() + "x" + M.ToString();
+            MWindow.ResizeMode = ResizeMode.CanMinimize;
+            MWindow.Height = N * PixelSize + 39 + 150;
+            MWindow.Width = M * PixelSize + 16;
+            MWindow.Padding = new Thickness(0);
+
+            Grid PixelGrid = new Grid();
+
+            PixelGrid.Height = N * PixelSize;
+            PixelGrid.Width = M * PixelSize;
+            PixelGrid.HorizontalAlignment = HorizontalAlignment.Center;
+            PixelGrid.VerticalAlignment = VerticalAlignment.Center;
+            PixelGrid.ShowGridLines = true;
+            PixelGrid.Margin = new Thickness(0);
+
+            RowDefinition[] row = new RowDefinition[N];
+            ColumnDefinition[] column = new ColumnDefinition[M];
+
+            for (int i = 0; i < N; i++)
+            {
+                row[i] = new RowDefinition();
+                PixelGrid.RowDefinitions.Add(row[i]);
+            }
+
+            for (int i = 0; i < M; i++)
+            {
+                column[i] = new ColumnDefinition();
+                PixelGrid.ColumnDefinitions.Add(column[i]);
+            }
+
+            button = new Button[N, M];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    button[i, j] = new Button();
+
+                    button[i, j].Width = PixelSize;
+                    button[i, j].Height = PixelSize;
+                    button[i, j].Margin = new Thickness(0);
+                    button[i, j].Padding = new Thickness(0);
+                    button[i, j].BorderThickness = new Thickness(0);
+                    button[i, j].Background = new SolidColorBrush(Colors.White);
+                    button[i, j].Click += this.pixelButtonClicked;
+                    button[i, j].Name = "pixel_" + i.ToString() + "_" + j.ToString();
+
+                    Grid.SetRow(button[i, j], i);
+                    Grid.SetColumn(button[i, j], j);
+
+                    PixelGrid.Children.Add(button[i, j]);
+                }
+            }
+
+            Grid MainGrid = new Grid();
+
+            RowDefinition pixelRow = new RowDefinition();
+            RowDefinition buttonRow = new RowDefinition();
+
+            pixelRow.Height = GridLength.Auto;
+
+            buttonRow.MinHeight = 150;
+            buttonRow.MaxHeight = 150;
+
+            MainGrid.RowDefinitions.Add(pixelRow);
+            MainGrid.RowDefinitions.Add(buttonRow);
+
+            Grid.SetRow(PixelGrid, 0);
+
+            Grid ButtonGrid = new Grid();
+
+            ButtonGrid.HorizontalAlignment = HorizontalAlignment.Center;
+            ButtonGrid.VerticalAlignment = VerticalAlignment.Center;
+
+            TextBlock buttonText = new TextBlock();
+
+            buttonText.Text = "Uruchom sieć";
+            buttonText.FontWeight = FontWeights.ExtraBold;
+            buttonText.FontSize = N + M;
+
+            Button runNetworkButton = new Button();
+
+            runNetworkButton.Name = "runNetworkButton";
+            runNetworkButton.Content = buttonText;
+            runNetworkButton.Padding = new Thickness(10);
+            runNetworkButton.Height = 130;
+            runNetworkButton.Width = (M * PixelSize) - 20;
+            runNetworkButton.Click += runNetworkButtonClick;
+
+            ButtonGrid.Children.Add(runNetworkButton);
+
+            Grid.SetRow(ButtonGrid, 1);
+
+            MainGrid.Children.Add(PixelGrid);
+            MainGrid.Children.Add(ButtonGrid);
+
+            MWindow.Content = MainGrid;
+        }
+
+        private void runNetworkButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            pixels = new bool[N, M];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    pixels[i, j] = false;
+                }
+            }
+
+            buildStructure();
+        }
+    }
+}
---
+++ HopfieldSSEditor/Matrix.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace HopfieldSS_Tools
+{
+    internal class Matrix
+    {
+        public double[,] rawData { get; private set; }
+        public int Rows { get; private set; }
+        public int Columns { get; private set; }
+
+        public void setElement(int row, int column, double value)
+        {
+            rawData[row, column] = value;
+        }
+
+        public double getElement(int row, int column)
+        {
+            return rawData[row, column];
+        }
+
+        public void print()
+        {
+            for (int i = 0; i < this.Rows; i++)
+            {
+                for (int j = 0; j < this.Columns; j++)
+                {
+                    Console.Write(rawData[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public Matrix()
+        {
+            this.Rows = 0;
+            this.Columns = 0;
+            rawData = new double[this.Rows, this.Columns];
+        }
+
+        public Matrix(int rows, int columns)
+        {
+            this.Rows = rows;
+            this.Columns = columns;
+            rawData = new double[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    rawData[i, j] = 0;
+                }
+            }
+        }
+
+        public static Matrix operator *(Matrix firstMatrix, Matrix secondMatrix)
+        {
+            if (firstMatrix.Columns == secondMatrix.Rows)
+            {
+                Matrix result = new Matrix(firstMatrix.Rows, secondMatrix.Columns);
+
+                for (int i = 0; i < firstMatrix.Rows; i++)
+                {
+                    for (int j = 0; j < secondMatrix.Columns; j++)
+                    {
+                        for (int k = 0; k < secondMatrix.Rows; k++)
+                        {
+                            double oldVal = result.getElement(i, j);
+                            result.setElement(i, j, oldVal + firstMatrix.getElement(i, k) * secondMatrix.getElement(k, j));
+                        }
+                    }
+                }
+
+                return result;
+            }
+            else
+            {
+                throw new ArgumentException("First Matrix column count must be equal to second Matrix row count!");
+            }
+        }
+
+        public static Matrix operator *(double number, Matrix matrix)
+        {
+            Matrix result = new Matrix(matrix.Rows, matrix.Columns);
+
+            for (int i = 0; i < matrix.Rows; i++)
+            {
+                for (int j = 0; j < matrix.Columns; j++)
+                {
+                    result.setElement(i, j, number * matrix.getElement(i, j));
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix operator *(Matrix matrix, double number)
+        {
+            Matrix result = new Matrix(matrix.Rows, matrix.Columns);
+
+            for (int i = 0; i < matrix.Rows; i++)
+            {
+                for (int j = 0; j < matrix.Columns; j++)
+                {
+                    result.setElement(i, j, number * matrix.getElement(i, j));
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix operator +(Matrix firstMatrix, Matrix secondMatrix)
+        {
+            if ((firstMatrix.Rows == secondMatrix.Rows) && (firstMatrix.Columns == secondMatrix.Columns))
+            {
+                Matrix result = new Matrix(firstMatrix.Rows, secondMatrix.Columns);
+
+                for (int i = 0; i < secondMatrix.Rows; i++)
+                {
+                    for (int j = 0; j < firstMatrix.Columns; j++)
+                    {
+                        result.setElement(i, j, firstMatrix.getElement(i, j) + secondMatrix.getElement(i, j));
+                    }
+                }
+
+                return result;
+            }
+            else
+            {
+                throw new ArgumentException("Matrix dimensions must be equal!");
+            }
+        }
+
+        public static Matrix operator -(Matrix firstMatrix, Matrix secondMatrix)
+        {
+            if ((firstMatrix.Rows == secondMatrix.Rows) && (firstMatrix.Columns == secondMatrix.Columns))
+            {
+                Matrix result = new Matrix(firstMatrix.Rows, secondMatrix.Columns);
+
+                for (int i = 0; i < secondMatrix.Rows; i++)
+                {
+                    for (int j = 0; j < firstMatrix.Columns; j++)
+                    {
+                        result.setElement(i, j, firstMatrix.getElement(i, j) - secondMatrix.getElement(i, j));
+                    }
+                }
+
+                return result;
+            }
+            else
+            {
+                throw new ArgumentException("Matrix dimensions must be equal!");
+            }
+        }
+    }
+}
